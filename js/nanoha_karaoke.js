7cd154167d88bf7687c6441edf1b598b86391658
sakurayoru <hiroseshun2000@gmail.com>
2020-01-26 14:30:04 +0900

    [add]search system

---
+++ js/nanoha_karaoke.js
@@ -0,0 +1,23 @@
+var requestURL = '../json/nanoha_song.json'
+var request = new XMLHttpRequest()
+request.open('GET', requestURL)
+request.responseType = 'json'
+request.send()
+
+function song_button(){
+    let id="",use="",ok=""
+    var result = request.response
+    id =document.getElementById("device").selectedIndex
+    console.log("id="+id)
+    var result = request.response
+    var nor = Object.keys(result).length
+    console.log("nor="+nor)
+    for (let i = 0; i < nor; i++) {
+        const j = result[i].use // use 取り出し
+        let k = j.match(/.{1}/g) // 1桁区切り
+        if (k[id] == 1) {
+            ok += result[i].song_name
+        }
+        document.getElementById("songs").innerHTML = ok
+    }
+}
